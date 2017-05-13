6afe2c4e98cf74adb4867a1aa4831a6aa9423772
cristian3140421 <marinhoyoscristianandres@gmail.com>
2017-05-13 14:49:18 -0500

    particion de css
    particion de css

--- Index.php
+++ Index.php
@@ -5,38 +5,7 @@
 	<!-- sirve para indicar al navegador que trabajaremos con responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<style type="text/css">
-		body{
-			background-color: #3b5798;
-		}
-		.down{
-			margin-top: 170px;
-		}
-		.downcolor{
-			background-color: #3b5798;
-		}
-		.btnentrar{
-			margin-top: 10px;
-			font-weight: bold;
-		}
-		.labelcolorwhite{
-			font-weight: bold;
-			font-size: 16px;
-		}
-		.labelbehind{
-			margin-top: -10px;
-		}
-		.labelatras{
-			z-index: -1;
-		}
-		.Login{
-			margin-top: -10px;
-			border-radius: 5px;
-			background-color: #f5f5f5;
-			padding: 10px;
-			width: 150px;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/Style.css">
 	<title>Login</title>
 </head>
 <body>
